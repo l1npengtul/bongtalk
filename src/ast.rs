43a66bf59b0eaf0451b318cbc99ca34edb7e9247
l1npengtul <l1npengtul@protonmail.com>
2022-10-17 00:41:11 +0900

    start making ast, update bilf pest

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,40 @@
-pub enum BILFAst {
-    Identifier(String),
-    SubaccessIdentifier(Vec<String>),
-    NamespacedIdentifier(Vec<String>),
-    Integer(i32),
-    Float(f32),
-    String(String),
+use crate::values::{CharacterString, Value};
+
+pub struct Identifier {
+    name: Vec<String>,
 }
+
+pub struct Import {
+    import: Option<Vec<Identifier>>,
+    from: String,
+}
+
+pub struct ChoiceOption {
+    value: Value,
+    rhai_expression: Expression,
+}
+
+pub struct FunctionDecleration {
+    public: bool,
+    recorded: bool,
+    name: Identifier,
+    body: Block,
+}
+
+pub enum Expression {
+    Literal(Value),
+    Rhai(String),
+}
+
+pub enum Statement {
+    Import(Import),
+    Comment(String),
+    FunctionDecl(FunctionDecleration),
+    ChoiceDecl {
+        choice: CharacterString,
+        options: Vec<ChoiceOption>,
+    },
+    RaiseEvent(Value),
+}
+
+pub type Block = Vec<Statement>;
